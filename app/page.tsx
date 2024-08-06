37b2fda0f1f57ed7312937eea897f26d29280316
Matheus Marqiues <99177444matheus@gmail.com>
2024-08-05 22:17:37 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return <div>
@@ -25,6 +28,29 @@ const Home = () => {
         fill 
         className="rounded-xl object-cover"/>
       </div>
+      <h2 className="mt-6 mb-3 text-sm uppercase font-bold text-gray-400">Agendamentos</h2>
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-semibold">Corte de cabelo</h3>
+
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                  </Avatar>
+
+                  <p className="text-sm">Barbearia FSW</p>
+                </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">05</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   </div>
 }
